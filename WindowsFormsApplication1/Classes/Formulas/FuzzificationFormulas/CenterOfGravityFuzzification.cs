972ecf6b5fe9ffca33c6559114b60b3d4d1ee7fb
exelim <as.exelim@mail.ru>
2014-12-04 14:05:22 +0200

    Fuzzifications formulas added

---
+++ WindowsFormsApplication1/Classes/Formulas/FuzzificationFormulas/CenterOfGravityFuzzification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Classes.Formulas.FuzzificationFormulas
+{
+    class CenterOfGravityFuzzification : FuzzificationFormulaBase
+    {
+        public override double CalculateFuzzification(double _val_1, double _val_2)
+        {
+            
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Classes/Formulas/FuzzificationFormulas/FuzzificationFormulaBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Classes.Formulas.FuzzificationFormulas
+{
+    abstract class FuzzificationFormulaBase
+    {
+        public abstract double CalculateFuzzification(double _val_1, double _val_2);
+    }
+}
