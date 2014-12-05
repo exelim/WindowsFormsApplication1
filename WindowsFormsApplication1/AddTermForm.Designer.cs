a6fff0aa21dd71dc14cf9fd2a7f4e0ad755c50bc
exelim <as.exelim@mail.ru>
2014-12-05 16:58:11 +0200

    save

---
+++ WindowsFormsApplication1/AddTermForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class AddTermForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TermsCountConfirmButton = new System.Windows.Forms.Button();
+            this.TermsCountUpDown = new System.Windows.Forms.NumericUpDown();
+            this.termsCountLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TermsCountUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TermsCountConfirmButton
+            // 
+            this.TermsCountConfirmButton.Location = new System.Drawing.Point(136, 20);
+            this.TermsCountConfirmButton.Name = "TermsCountConfirmButton";
+            this.TermsCountConfirmButton.Size = new System.Drawing.Size(75, 19);
+            this.TermsCountConfirmButton.TabIndex = 6;
+            this.TermsCountConfirmButton.Text = "Confirm";
+            this.TermsCountConfirmButton.UseVisualStyleBackColor = true;
+            this.TermsCountConfirmButton.Click += new System.EventHandler(this.TermsCountConfirmButton_Click);
+            // 
+            // TermsCountUpDown
+            // 
+            this.TermsCountUpDown.Location = new System.Drawing.Point(93, 20);
+            this.TermsCountUpDown.Name = "TermsCountUpDown";
+            this.TermsCountUpDown.Size = new System.Drawing.Size(36, 20);
+            this.TermsCountUpDown.TabIndex = 5;
+            this.TermsCountUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // termsCountLabel
+            // 
+            this.termsCountLabel.AutoSize = true;
+            this.termsCountLabel.Location = new System.Drawing.Point(21, 20);
+            this.termsCountLabel.Name = "termsCountLabel";
+            this.termsCountLabel.Size = new System.Drawing.Size(66, 13);
+            this.termsCountLabel.TabIndex = 4;
+            this.termsCountLabel.Text = "Terms count";
+            // 
+            // AddTermForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 356);
+            this.Controls.Add(this.TermsCountConfirmButton);
+            this.Controls.Add(this.TermsCountUpDown);
+            this.Controls.Add(this.termsCountLabel);
+            this.Name = "AddTermForm";
+            this.Text = "AddTermForm";
+            ((System.ComponentModel.ISupportInitialize)(this.TermsCountUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TermsCountConfirmButton;
+        private System.Windows.Forms.NumericUpDown TermsCountUpDown;
+        private System.Windows.Forms.Label termsCountLabel;
+    }
+}
--- WindowsFormsApplication1/AddTermForm.cs
+++ WindowsFormsApplication1/AddTermForm.cs
@@ -8,43 +8,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using WindowsFormsApplication1.Classes;
-using WindowsFormsApplication1.Structures;
-
 namespace WindowsFormsApplication1
 {
-    enum VariableType { IN, OUT };
-    enum RelationType { AND, OR };
-
-    public partial class Form1 : Form
+    public partial class AddTermForm : Form
     {
-        public int lexicalVariablesCount;
+        public int termsCount;
 
-        public Form1()
+        public AddTermForm()
         {
             InitializeComponent();
         }
 
-        private void LVCountConfirmButton_Click(object sender, EventArgs e)
+        private void TermsCountConfirmButton_Click(object sender, EventArgs e)
         {
-            if (lexicalVariablesCount > Convert.ToInt32(LVCountUpDown.Value))
+            if (termsCount > Convert.ToInt32(TermsCountUpDown.Value))
             {
-                for (int i = 0; i <= lexicalVariablesCount; i++)
+                for (int i = 0; i <= termsCount; i++)
                 {
-                    this.Controls.RemoveByKey( "LiguisticVariable_" + (i + 1) );
+                    this.Controls.RemoveByKey("LiguisticVariable_" + (i + 1));
                 }
             }
 
-            lexicalVariablesCount = Convert.ToInt32(LVCountUpDown.Value);
+            termsCount = Convert.ToInt32(TermsCountUpDown.Value);
 
-            for (int i = 0; i < LVCountUpDown.Value; i++)
+            for (int i = 0; i < TermsCountUpDown.Value; i++)
             {
                 // creating Label LV number
                 Label Numberlabel = new Label();
                 Numberlabel.Name = "label_LVNumber_" + (i + 1);
                 Numberlabel.Text = "Linguistic var №" + (i + 1) + ":";
                 Numberlabel.Width = 95;
-                Numberlabel.Location = new Point(LVCountLabel.Location.X, LVCountLabel.Location.Y + ( 25 + 25 * i));
+                Numberlabel.Location = new Point(termsCountLabel.Location.X, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(Numberlabel);
 
                 // creating Label LV ID
@@ -52,14 +46,14 @@ namespace WindowsFormsApplication1
                 IDlabel.Name = "label_LVID_" + (i + 1);
                 IDlabel.Text = "Id:";
                 IDlabel.Width = 20;
-                IDlabel.Location = new Point(Numberlabel.Location.X + Numberlabel.Width, LVCountLabel.Location.Y + (25 + 25 * i));
+                IDlabel.Location = new Point(Numberlabel.Location.X + Numberlabel.Width, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(IDlabel);
 
-                 //creating TextBox for LV id
+                //creating TextBox for LV id
                 TextBox IDtextBox = new TextBox();
                 IDtextBox.Name = "textbox_LVID_" + (i + 1);
                 IDtextBox.Width = 50;
-                IDtextBox.Location = new Point(IDlabel.Location.X + IDlabel.Width, LVCountLabel.Location.Y + (25 + 25 * i));
+                IDtextBox.Location = new Point(IDlabel.Location.X + IDlabel.Width, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(IDtextBox);
 
                 // creating Label LV name
@@ -67,14 +61,14 @@ namespace WindowsFormsApplication1
                 Namelabel.Name = "label_LVName_" + (i + 1);
                 Namelabel.Text = "Linguistic var name:";
                 Namelabel.Width = 110;
-                Namelabel.Location = new Point(IDtextBox.Location.X + IDtextBox.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
+                Namelabel.Location = new Point(IDtextBox.Location.X + IDtextBox.Width + 5, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(Namelabel);
 
                 // creating TextBox for LV name
                 TextBox NametextBox = new TextBox();
                 NametextBox.Name = "textbox_LVName_" + (i + 1);
                 NametextBox.Width = 100;
-                NametextBox.Location = new Point(Namelabel.Location.X + Namelabel.Width, LVCountLabel.Location.Y + (25 + 25 * i));
+                NametextBox.Location = new Point(Namelabel.Location.X + Namelabel.Width, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(NametextBox);
 
                 // creating Label LV min range
@@ -82,14 +76,14 @@ namespace WindowsFormsApplication1
                 MinRangelabel.Name = "label_LVMinrange_" + (i + 1);
                 MinRangelabel.Text = "Range from:";
                 MinRangelabel.Width = 65;
-                MinRangelabel.Location = new Point(NametextBox.Location.X + NametextBox.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
+                MinRangelabel.Location = new Point(NametextBox.Location.X + NametextBox.Width + 5, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(MinRangelabel);
 
                 // creating UpDown LV min range
                 NumericUpDown MinRangeUpDown = new NumericUpDown();
                 MinRangeUpDown.Name = "upDown_LVMinrange_" + (i + 1);
                 MinRangeUpDown.Width = 36;
-                MinRangeUpDown.Location = new Point(MinRangelabel.Location.X + MinRangelabel.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
+                MinRangeUpDown.Location = new Point(MinRangelabel.Location.X + MinRangelabel.Width + 5, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(MinRangeUpDown);
 
                 // creating Label LV max range
@@ -97,14 +91,14 @@ namespace WindowsFormsApplication1
                 MaxRangelabel.Name = "label_LVMaxrange_" + (i + 1);
                 MaxRangelabel.Text = "Range to:";
                 MaxRangelabel.Width = 60;
-                MaxRangelabel.Location = new Point(MinRangeUpDown.Location.X + MinRangeUpDown.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
+                MaxRangelabel.Location = new Point(MinRangeUpDown.Location.X + MinRangeUpDown.Width + 5, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(MaxRangelabel);
 
                 // creating UpDown LV max range
                 NumericUpDown MaxRangeUpDown = new NumericUpDown();
                 MaxRangeUpDown.Name = "upDown_LVMaxrange_" + (i + 1);
                 MaxRangeUpDown.Width = 36;
-                MaxRangeUpDown.Location = new Point(MaxRangelabel.Location.X + MaxRangelabel.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
+                MaxRangeUpDown.Location = new Point(MaxRangelabel.Location.X + MaxRangelabel.Width + 5, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(MaxRangeUpDown);
 
                 // creating Label Terms count
@@ -112,31 +106,11 @@ namespace WindowsFormsApplication1
                 TermsCountlabel.Name = "label_LVMaxrange_" + (i + 1);
                 TermsCountlabel.Text = "Terms count:";
                 TermsCountlabel.Width = 70;
-                TermsCountlabel.Location = new Point(MaxRangeUpDown.Location.X + MaxRangeUpDown.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
+                TermsCountlabel.Location = new Point(MaxRangeUpDown.Location.X + MaxRangeUpDown.Width + 5, termsCountLabel.Location.Y + (25 + 25 * i));
                 this.Controls.Add(TermsCountlabel);
-
-                // creating UpDown terms Count
-                NumericUpDown TermsCountUpDown = new NumericUpDown();
-                TermsCountUpDown.Name = "upDown_TermsCount_" + (i + 1);
-                TermsCountUpDown.Width = 36;
-                TermsCountUpDown.Location = new Point(TermsCountlabel.Location.X + TermsCountlabel.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
-                this.Controls.Add(TermsCountUpDown);
-
-                // creating button add terms
-                Button AddTerms = new Button();
-                AddTerms.Name = "button_AddTerms_" + (i + 1);
-                AddTerms.Width = 90;
-                AddTerms.Text = "Add terms";
-                AddTerms.Location = new Point(TermsCountUpDown.Location.X + TermsCountUpDown.Width + 5, LVCountLabel.Location.Y + (25 + 25 * i));
-                AddTerms.Click += AddTermsButton_Clicked;
-                this.Controls.Add(AddTerms);
-            }          
+            }
         }
 
-        void AddTermsButton_Clicked(object sender, EventArgs e)
-        {
-            Form addTermsForm = new AddTermForm();
-            addTermsForm.Show();
-        }
+
     }
 }
