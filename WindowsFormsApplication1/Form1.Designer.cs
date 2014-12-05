e538bac9dd21c53b2680a6589f91062d72c0df7c
exelim <as.exelim@mail.ru>
2014-12-05 12:13:45 +0200

    new changes

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LVCountLabel = new System.Windows.Forms.Label();
+            this.LVCountUpDown = new System.Windows.Forms.NumericUpDown();
+            this.LVCountConfirmButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.LVCountUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LVCountLabel
+            // 
+            this.LVCountLabel.AutoSize = true;
+            this.LVCountLabel.Location = new System.Drawing.Point(12, 23);
+            this.LVCountLabel.Name = "LVCountLabel";
+            this.LVCountLabel.Size = new System.Drawing.Size(126, 13);
+            this.LVCountLabel.TabIndex = 0;
+            this.LVCountLabel.Text = "Linguistic variables count";
+            // 
+            // LVCountUpDown
+            // 
+            this.LVCountUpDown.Location = new System.Drawing.Point(144, 21);
+            this.LVCountUpDown.Name = "LVCountUpDown";
+            this.LVCountUpDown.Size = new System.Drawing.Size(36, 20);
+            this.LVCountUpDown.TabIndex = 2;
+            // 
+            // LVCountConfirmButton
+            // 
+            this.LVCountConfirmButton.Location = new System.Drawing.Point(187, 21);
+            this.LVCountConfirmButton.Name = "LVCountConfirmButton";
+            this.LVCountConfirmButton.Size = new System.Drawing.Size(75, 19);
+            this.LVCountConfirmButton.TabIndex = 3;
+            this.LVCountConfirmButton.Text = "Confirm";
+            this.LVCountConfirmButton.UseVisualStyleBackColor = true;
+            this.LVCountConfirmButton.Click += new System.EventHandler(this.LVCountConfirmButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 315);
+            this.Controls.Add(this.LVCountConfirmButton);
+            this.Controls.Add(this.LVCountUpDown);
+            this.Controls.Add(this.LVCountLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LVCountUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LVCountLabel;
+        private System.Windows.Forms.NumericUpDown LVCountUpDown;
+        private System.Windows.Forms.Button LVCountConfirmButton;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -24,5 +24,34 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void LVCountConfirmButton_Click(object sender, EventArgs e)
+        {
+            if (lexicalVariablesCount > Convert.ToInt32(LVCountUpDown.Value))
+            {
+                for (int i = 0; i <= lexicalVariablesCount; i++)
+                {
+                    this.Controls.RemoveByKey( "LiguisticVariable_" + (i + 1) );
+                }
+            }
+
+            lexicalVariablesCount = Convert.ToInt32(LVCountUpDown.Value);
+
+            for (int i = 0; i <= LVCountUpDown.Value; i++)
+            {
+                // creating Label name LV number
+                Label label = new Label();
+                label.Name = "LiguisticVariable_" + (i + 1);
+                label.Text = "Linguistic variable №" + (i + 1) + ":";
+                label.Location = new Point(LVCountLabel.Location.X, LVCountLabel.Location.Y + (25 * i));
+                this.Controls.Add(label);
+
+                // creating TextBox for LV id
+                TextBox textBox = new TextBox();
+                textBox.Name = "LVId_" + (i + 1);
+                textBox.Location = 
+
+            }
+        }
     }
 }
